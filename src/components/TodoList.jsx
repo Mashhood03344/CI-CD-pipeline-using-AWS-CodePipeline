e2c9632c05ae5c71cad6ade99e57b12662181676
Mash-hood <mashhoodhamid33@gmail.com>
2024-08-06 00:05:35 +0500

    Made Changes in the TodoList.jsx file

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,7 +3,7 @@ import TodoItem from "./TodoItem";
 const TodoList = ({ todos, remove, update }) => {
   return (
     <div className="list-container">
-      <h2>Todos List</h2>
+      <h2>Here The TODO List will be Shown</h2>
       <div className="list">
         {todos.map((todo) => (
           <TodoItem
